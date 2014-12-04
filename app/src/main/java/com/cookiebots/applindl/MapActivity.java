8f715c1578a0eab71bd5fc527a5c74f924e80e45
Alexandre <alexandreplaitant@hotmail.fr>
2014-12-04 23:00:13 +0100

    Init Map

---
+++ app/src/main/java/com/cookiebots/applindl/MapActivity.java
@@ -0,0 +1,48 @@
+package com.cookiebots.applindl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by Alexandre on 04/12/2014.
+ */
+
+public class MapActivity extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.map_activity);
+
+            // Get a handle to the Map Fragment
+            GoogleMap map = ((MapFragment) getFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+
+            LatLng syrie = new LatLng(34.923081, 39.002507);
+
+            map.setMyLocationEnabled(true);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(syrie, 13));
+
+        }
+
+    private MarkerOptions makeMarker(double Lat, double Lng, String titre, String snip){
+        LatLng pos = new LatLng(Lat, Lng);
+        MarkerOptions marker = new MarkerOptions().title(titre).snippet(snip).position(pos);
+        return marker;
+    }
+
+    private CircleOptions makeCircle(LatLng center, float width){
+        CircleOptions circle = new CircleOptions();
+        circle.center(center);
+        circle.strokeWidth(width);
+        return circle;
+    }
+}
